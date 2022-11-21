d22eb8691624b45ddf195d92fd9868c3f6a3b57f
Jules Pognante <pognante.jules@gmail.com>
2022-11-21 19:38:16 +0100

    added display for element fetched from the API and styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
+import Card from "./components/Card";
 
 const App = () => {
     const [recettes, setRecettes] = useState([]);
     useEffect(() => {
-        axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=tomato")
+        axios.post("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
             .then((result) => {
                setRecettes(result.data["meals"]);
             })
     }, []);
+
     return (
         <div>
             <div id={"head"} className={"text-center mt-4 d-flex flex-column align-items-center"}>
-                <h1 className={"mb-4"}>Appli recettes de cuisine</h1>
+                <h1 className={"mb-4"} style={{"color":"#f88577"}}>Appli recettes de cuisine</h1>
                 <input type="text" className="form-control w-25" placeholder="Tapez le nom d'un aliment (en anglais)" />
+            </div>
+            <div className={"row justify-content-center mt-4"}>
+                {recettes.map(element => (
+                    <Card key={element.idMeal}
+                          details={{
+                                      name: element.strMeal,
+                                      origin: element.strArea,
+                                      img: element.strMealThumb,
+                                      desc: element.strInstructions
+                                  }}
+                    />
+                    ))}
             </div>
         </div>
     );
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Card = ({details}) => {
     return (
-        <div>
-
+        <div className={"card col-2 me-4"} style={{"borderColor":"#f88577"}}>
+            <div className={"card-body"}>
+                <h5 className={"card-title text-center"}>{details.name}</h5>
+                <p className={"card-text text-center"}>Origin : {details.origin} </p>
+                <img src={details.img} style={{"maxWidth":"100%", "borderRadius":"10px"}} alt={"img"}/>
+                <p className={"card-text mt-4 mb-4"}
+                   style={{
+                       "display": "-webkit-box",
+                       "WebkitLineClamp": "8",
+                       "WebkitBoxOrient": "vertical",
+                       "overflow": "hidden"
+                   }}
+                >{details.desc}</p>
+            </div>
         </div>
     );
 };
