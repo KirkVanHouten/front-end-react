59d1062e9f6539fe0c05c34ec7f63d72f78be455
KirkVanHouten <pognante.jules@gmail.com>
2022-11-21 15:25:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [recettes, setRecettes] = useState([]);
+    useEffect(() => {
+        axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=tomato")
+            .then((result) => {
+               setRecettes(result.data["meals"]);
+            })
+    }, []);
+    return (
+        <div>
+            <div id={"head"} className={"text-center mt-4 d-flex flex-column align-items-center"}>
+                <h1 className={"mb-4"}>Appli recettes de cuisine</h1>
+                <input type="text" className="form-control w-25" placeholder="Tapez le nom d'un aliment (en anglais)" />
+            </div>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = ({details}) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Card;
