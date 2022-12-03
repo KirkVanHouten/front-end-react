90539a7b5b62b4b7107c75a98ae1e86344b1c016
Jules Pognante <pognante.jules@gmail.com>
2022-12-03 13:24:33 +0100

    final commit

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,15 +7,10 @@ const Menu = () => {
     };
     return (
         <div className={"d-flex justify-content-center"}>
-            <nav className="navbar navbar-expand-lg bg-light w-75">
+            <nav className="navbar bg-light w-75">
                 <div className="container-fluid">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                            aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav d-flex justify-content-center w-100">
+                    <div id="navbarNav">
+                        <ul className="navbar-nav d-flex flex-row w-100">
                             <li className="nav-item me-3 active">
                                 <NavLink to={"recettes"}
                                          className={"nav-link"}
